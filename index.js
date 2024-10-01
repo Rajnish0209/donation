ee345127a1974b03fadbfe6e2fcbae12d93bdda0
Rajnish0209 <rajnishkumargupta331@gmail.com>
2024-10-01 09:41:11 +0530

    add country in home.ejs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+let Country = require('country-state-city').Country;
+let State = require('country-state-city').State;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,8 +12,10 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+const countries = Country.getAllCountries();
+
 app.get("/home", (req, res) => {
-  res.render("home.ejs");
+  res.render("home.ejs", { countries });
 });
 
 app.listen(8080, () => {

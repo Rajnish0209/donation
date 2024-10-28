64cb3a732d714ee15328378f6ab2b96949a3d454
Rajnish0209 <rajnishkumargupta331@gmail.com>
2024-10-28 23:18:13 +0530

    create script.js and home.ejs in public and views folder. install country-state-city npm package

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
 app.get("/home", (req, res) => {
-  res.send("Hello World");
+  res.render("home.ejs");
 });
 
 app.listen(port, () => {
